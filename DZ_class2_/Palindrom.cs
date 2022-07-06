efaefaecfc467b1d0dd29c7875f573a10c91fd11
GinOfSand <96676512+GinOfSand@users.noreply.github.com>
2022-07-06 14:46:06 +0700

    Добавьте файлы проекта.

---
+++ DZ_class2_/Palindrom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_class2_
+{
+    internal class Palindrom
+    {
+        public int number { get; set; }
+        public Palindrom(int numbers = 1111) { number = numbers; }
+
+        public bool CheckP()
+        {
+            int [] chekers = new int[number.ToString().Length];
+            for(int i = 0; i < number.ToString().Length; i++)
+            {
+                chekers[i] = number.ToString()[number.ToString().Length-i-1] -48;
+            }
+            int che = 0;
+            int porydok = Convert.ToInt32(Math.Pow(10,Convert.ToDouble(chekers.Length)-1));
+            for(int i = 0; i < chekers.Length; i++)
+            {
+                che += chekers[i] * porydok;
+                porydok /= 10;
+            }
+            return number == che;
+        }
+    }
+}
---
+++ DZ_class2_/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_class2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число узнаем палиндром или нет: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Palindrom b = new Palindrom(a);
+            Console.WriteLine(b.CheckP());
+        }
+    }
+}
